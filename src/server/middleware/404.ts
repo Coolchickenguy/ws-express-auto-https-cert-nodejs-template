426b3133aa352f67a7e51a3363aa904e55310699
Coolchickenguy <152336774+Coolchickenguy@users.noreply.github.com>
2024-08-24 18:44:22 -0600

    Update 404.ts - remove unused event emitter

--- src/server/middleware/404.ts
+++ src/server/middleware/404.ts
@@ -1,10 +1,8 @@
 import * as express from "express";
 import {resolve} from "path";
 import type { wsRouter } from "../wsRouter.js";
-import EventEmitter from "events";
 
 export default async function main(app:express.Express,ws:wsRouter):Promise<void>{
-    let emitter = new EventEmitter();
     const pathOf404Html = resolve("assets/private/404.html");
     app.use(function(req,res){
         res.status(404);
